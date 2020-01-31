dc269b5cacfb9895f902af3a36eb6f20ee56ef4d
agreen78 <andrewgreen78@gmail.com>
2020-01-31 15:02:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserInputAndrewGreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display greeting
+            Console.WriteLine("Hi, this is Andrew Green.");
+            Console.ReadKey(true);
+        }
+    }
+}
